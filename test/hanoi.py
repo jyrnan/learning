cfc6312b6cadfca5615ec919ebac743f58c9dea1
jyrnan <yoeking@gmail.com>
2018-09-07 16:15:21 +0800

    modified!

--- test/hanoi.py
+++ test/hanoi.py
@@ -3,14 +3,14 @@ def hanoi(n, x, y, z):
 	global step
 	if n == 1:
 		step.append(x+'>'+z)
-		#print(str(x), ' > ', str(z))
+		print(str(x), ' > ', str(z))
 	else:
 		hanoi((n-1), x, z, y)
 		step.append(x+'>'+z)
-		#print(str(x), ' > ', str(z))
+		print(str(x), ' > ', str(z))
 		hanoi((n-1), y, x, z)
 		
 	return step
-step = hanoi(25, 'x', 'y', 'z')
+step = hanoi(5, 'x', 'y', 'z')
 print(len(step))
 
