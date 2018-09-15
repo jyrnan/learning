01759cc91681599d29336fccaaa4a89d9b1aa75c
yoeking <yoeking@gmail.com>
2018-09-15 17:23:27 +0800

    modified!

---
+++ cookbook/8_class/8_2_supper.py
@@ -0,0 +1,16 @@
+class A():
+    def spam(self):
+        print('A.spam')
+        super().spam()
+
+class B():
+    def spam(self):
+        print('B.spam')
+
+
+class C(A,B):
+    pass
+
+c = C()
+c.spam()
+print(type(C), '\n', dir(C))
